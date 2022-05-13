d2ca68889eae53c1fd616c4d72c171c14c1b55d6
Andy Hu <huandy01@gmail.com>
2022-05-13 23:50:47 +0200

    Button style + Font

--- hulongrr/src/Startseite/Startseite.jsx
+++ hulongrr/src/Startseite/Startseite.jsx
@@ -5,13 +5,13 @@ export default function Startseite() {
     return (
         <div className="startseiteContainer">
             <div>
-                <h2>Hulong RR - Junior Entwickler</h2>
+                <h2>Hulong RR - <span>Junior Entwickler</span></h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate dolor fugiat necessitatibus magnam consectetur. Ducimus magni at, aperiam error, aspernatur consequatur tenetur, explicabo iste minus velit deleniti assumenda ab!</p>
-                <div>Mehr erfahren</div>
+                <div className="mehrErfahren">Mehr erfahren</div>
             </div>
             <div>
                 <div className="hulongRRDiv">
-                    <div class="gradient blurry"/>
+                    <div className="gradient blurry"/>
                     <img src={HulongRR} alt="Hulong RR Profil" />
                 </div>
             </div>
