61cb62032d7b4527bf5b79783e67f5c1695602fd
Andy Hu <huandy01@gmail.com>
2022-05-14 02:25:33 +0200

    Message

--- hulongrr/src/Startseite/Startseite.jsx
+++ hulongrr/src/Startseite/Startseite.jsx
@@ -6,13 +6,13 @@ export default function Startseite() {
         <div className="startseiteContainer">
             <div>
                 <p className="hiIchBin">Hi, ich bin</p>
-                <h2>Hulong RR - <span>Junior Entwickler</span></h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In cupiditate dolor fugiat necessitatibus magnam consectetur. Ducimus magni at, aperiam error, aspernatur consequatur tenetur, explicabo iste minus velit deleniti assumenda ab!</p>
+                <h2>Hulong RR - <span onClick={() => window.open("https://devskiller.com/de/junior-entwickler/#:~:text=Kandidaten%20f%C3%BCr%20eine%20Junior%2DEntwickler,der%20Erstellung%20von%20Software%20haben")}>Junior Developer</span></h2>
+                <p>Seit 2018 gehe ich diese Tätigkeit privat, sowie 2019 beruflich an: Coden. Jeder Anfang ist schwer, aber es lohnt sich dran zu bleiben und die Erfahrungen mitzunehmen. Seit dieser Zeit, bin ich von Tag eins mit Leidenschaft dabei und werde nie ausgelernt haben. #CodingIsFuture</p>
                 <ul>
-                    <li><i class="fa fa-brands fa-instagram"></i></li>
-                    <li><i class="fa fa-brands fa-github"></i></li>
-                    <li><i class="fa fa-brands fa-youtube"></i></li>
-                    <li><i class="fa fa-brands fa-twitter"></i></li>
+                    <li onClick={() => window.open("https://www.instagram.com/hulong_rr")}><i class="fa fa-brands fa-instagram"></i></li>
+                    <li onClick={() => window.open("https://github.com/AndyHu8")}><i class="fa fa-brands fa-github"></i></li>
+                    <li onClick={() => window.open("https://www.youtube.com/channel/UCm_LK9R9h0h_Kw6MWG3H7RQ")}><i class="fa fa-brands fa-youtube"></i></li>
+                    <li onClick={() => window.open("https://twitter.com/Hulong_RR")}><i class="fa fa-brands fa-twitter"></i></li>
                 </ul>
                 <div className="mehrErfahren">Mehr erfahren <i className="fa fa-solid fa-arrow-right"></i></div>
             </div>
