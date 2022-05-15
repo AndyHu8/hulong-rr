4c7c71601c275f34dfd8eb893efce5d5735be21a
Andy Hu <huandy01@gmail.com>
2022-05-15 17:39:34 +0200

    Nav Effekt

--- hulongrr/src/Navigation/Navigation.jsx
+++ hulongrr/src/Navigation/Navigation.jsx
@@ -5,11 +5,26 @@ export default function Navigation() {
         <div className="navigationContainer">
             <div>胡龙日日</div>
             <ul>
-                <li>Startseite</li>
-                <li>Über mich</li>
-                <li>Skills</li>
-                <li>Projekte</li>
-                <li>Kontakt</li>
+                <div>
+                    <li className="firstLi">Startseite</li>
+                    <li className="secondLi">主页</li>
+                </div>
+                <div>
+                    <li className="firstLi">Über mich</li>
+                    <li className="secondLi">关于我</li>
+                </div>
+                <div>
+                    <li className="firstLi">Skills</li>
+                    <li className="secondLi">技能</li>
+                </div>
+                <div>
+                    <li className="firstLi">Projekte</li>
+                    <li className="secondLi">项目</li>
+                </div>
+                <div>
+                    <li className="firstLi">Kontakt</li>
+                    <li className="secondLi">联系</li>
+                </div>
             </ul>
         </div>
     )
