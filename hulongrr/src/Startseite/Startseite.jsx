269b71fe7dc9e81a58e402188402d1941a43f2ff
Andy Hu <huandy01@gmail.com>
2022-05-15 16:44:43 +0200

    background change

--- hulongrr/src/Startseite/Startseite.jsx
+++ hulongrr/src/Startseite/Startseite.jsx
@@ -3,9 +3,9 @@ import HulongRR from "../Bilder/hulongrr.jpg"
 
 export default function Startseite() {
     return (
-        <div className="startseiteSection">
             <div className="startseiteContainer">
             <div>
+                <div className="chinStartseite">主页</div>
                 <p className="hiIchBin">Hi, ich bin</p>
                 <h2>Hulong RR - <span onClick={() => window.open("https://devskiller.com/de/junior-entwickler/#:~:text=Kandidaten%20f%C3%BCr%20eine%20Junior%2DEntwickler,der%20Erstellung%20von%20Software%20haben")}>Junior Developer</span></h2>
                 <p>Seit 2018 gehe ich diese Tätigkeit privat, sowie 2019 beruflich an: Coden. Jeder Anfang ist schwer, aber es lohnt sich dran zu bleiben und die Erfahrungen mitzunehmen. Seit dieser Zeit, bin ich von Tag eins mit Leidenschaft dabei und werde nie ausgelernt haben. #CodingIsFuture</p>
@@ -24,10 +24,6 @@ export default function Startseite() {
                     <img src={HulongRR} alt="Hulong RR Profil" onClick={() => window.location.reload()}/>
                 </div>
             </div>
-        </div>
-
-        {/*Chinese Font*/}
-        <div className="chineseFont">主页</div>
         </div>
     )
 }
