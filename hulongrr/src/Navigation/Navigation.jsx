d540965c3c8331326840d77beb9cc0a2a85053a9
Andy Hu <huandy01@gmail.com>
2022-05-16 22:55:40 +0200

    Nav mit transform RPV

--- hulongrr/src/Navigation/Navigation.jsx
+++ hulongrr/src/Navigation/Navigation.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "./Navigation.css"
 
 export default function Navigation() {
+    const [isNavOpen, setIsNavOpen] = useState(false);
     
     function showNav() {
         const MobileUl = document.querySelector(".mobileNav");
-        const MobileBg = document.querySelector(".mobileNavContainer");
-        MobileBg.style.display = "block";
-        MobileUl.style.display = "block";
+        if(!isNavOpen || "") {
+            MobileUl.style.display = "block";
+            setIsNavOpen(true);
+        }
+        else {
+            MobileUl.style.display = "none";
+            setIsNavOpen(false);
+        }
     }
 
     return (
@@ -38,28 +45,22 @@ export default function Navigation() {
             <div className="burger" onClick={showNav}><i className="fa fa-solid fa-bars"></i></div>
         </div>
 
-        <div className="mobileNavContainer"></div>
         <div className="mobileNav">
             <ul>
                 <div>
-                    <li className="firstLi">Startseite</li>
-                    <li className="secondLi">主页</li>
+                    <li className="firstLi">Startseite | 主页</li>
                 </div>
                 <div>
-                    <li className="firstLi">Über mich</li>
-                    <li className="secondLi">关于我</li>
+                    <li className="firstLi">Über mich | 关于我</li>
                 </div>
                 <div>
-                    <li className="firstLi">Skills</li>
-                    <li className="secondLi">技能</li>
+                    <li className="firstLi">Skills | 技能</li>
                 </div>
                 <div>
-                    <li className="firstLi">Projekte</li>
-                    <li className="secondLi">项目</li>
+                    <li className="firstLi">Projekte | 项目</li>
                 </div>
                 <div>
-                    <li className="firstLi">Kontakt</li>
-                    <li className="secondLi">联系</li>
+                    <li className="firstLi">Kontakt | 联系</li>
                 </div>
             </ul>
         </div>
