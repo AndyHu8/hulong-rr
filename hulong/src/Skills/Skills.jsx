19f160916b8761efb33cc444f33b2f35c0d8daa6
Andy Hu <huandy01@gmail.com>
2022-05-30 23:46:12 +0200

    Skills Abschnitt

--- hulong/src/Skills/Skills.jsx
+++ hulong/src/Skills/Skills.jsx
@@ -9,6 +9,16 @@ export default function Skills() {
                 <p className="aboutTextProjects">Hier sind meine Skills aufgelistet. Mit HTML habe ich gestartet und immer weiter im Frontend-Entwicklung ins Detail gegangen. Hierzu fehlt natürlich CSS nicht und auch JavaScript, sowie React. Dies sind die grundlegenden Skills, die man benötigt.
                 Hier unten findest du alle Skills aufgelistet.
                 </p>
+                <div className="alleSkillsContainer">
+                    <div onClick={() => window.open("https://www.w3schools.com/html/", "_blank")}>HTML</div>
+                    <div onClick={() => window.open("https://www.w3schools.com/css/", "_blank")}>CSS</div>
+                    <div onClick={() => window.open("https://www.w3schools.com/js/", "_blank")}>JavaScript</div>
+                    <div onClick={() => window.open("https://reactjs.org/", "_blank")}>React.js</div>
+                    <div onClick={() => window.open("https://www.typescriptlang.org/", "_blank")}>TypeScript</div>
+                    <div onClick={() => window.open("https://docs.microsoft.com/de-de/dotnet/csharp/tour-of-csharp/", "_blank")}>C#</div>
+                    <div onClick={() => window.open("https://geekflare.com/de/best-css-frameworks/", "_blank")}>CSS Frameworks</div>
+                    <div onClick={() => window.open("https://www.ionos.de/digitalguide/hosting/hosting-technik/was-ist-webhosting/", "_blank")}>Hosting</div>
+                </div>
             </div>
         </div>
     )
