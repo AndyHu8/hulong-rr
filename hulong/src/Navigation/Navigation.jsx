88507141f81c8664487c72d97ecf60caafbee16b
Andy Hu <huandy01@gmail.com>
2022-10-26 22:24:57 +0200

    Xiaohu Nav

--- hulong/src/Navigation/Navigation.jsx
+++ hulong/src/Navigation/Navigation.jsx
@@ -3,10 +3,10 @@ import "./Navigation.css"
 
 export default function Navigation() {
     const [isNavOpen, setIsNavOpen] = useState(false);
-    
+
     function showNav() {
         const MobileUl = document.querySelector(".mobileNav");
-        if(!isNavOpen || "") {
+        if (!isNavOpen || "") {
             MobileUl.style.display = "block";
             setIsNavOpen(true);
         }
@@ -18,52 +18,52 @@ export default function Navigation() {
 
     return (
         <>
-        <div className="navigationContainer">
-            <div onClick={() => window.location.reload()}><span>Hulong</span> 胡龙</div>
-            <ul>
-                <div>
-                    <a href="#StartseiteSection"><li className="firstLi">Startseite</li></a>
-                    <a href="#StartseiteSection"><li className="secondLi">主页</li></a>
-                </div>
-                <div>
-                    <a href="#AboutSection"><li className="firstLi">Über mich</li></a>
-                    <a href="#AboutSection"><li className="secondLi">关于我</li></a>
-                </div>
-                <div>
-                    <a href="#SkillsSection"><li className="firstLi">Skills</li></a>
-                    <a href="#SkillsSection"><li className="secondLi">技能</li></a>
-                </div>
-                <div>
-                    <a href="#ProjectsSection"><li className="firstLi">Projekte</li></a>
-                    <a href="#ProjectsSection"><li className="secondLi">项目</li></a>
-                </div>
-                <div>
-                    <a href="#KontaktSection"><li className="firstLi">Kontakt</li></a>
-                    <a href="#KontaktSection"><li className="secondLi">联系</li></a>
-                </div>
-            </ul>
-            <div className="burger" onClick={showNav}><i className="fa fa-solid fa-bars"></i></div>
-        </div>
+            <div className="navigationContainer">
+                <div onClick={() => window.location.reload()}><span>Xiaohu</span> 小胡</div>
+                <ul>
+                    <div>
+                        <a href="#StartseiteSection"><li className="firstLi">Startseite</li></a>
+                        <a href="#StartseiteSection"><li className="secondLi">主页</li></a>
+                    </div>
+                    <div>
+                        <a href="#AboutSection"><li className="firstLi">Über mich</li></a>
+                        <a href="#AboutSection"><li className="secondLi">关于我</li></a>
+                    </div>
+                    <div>
+                        <a href="#SkillsSection"><li className="firstLi">Skills</li></a>
+                        <a href="#SkillsSection"><li className="secondLi">技能</li></a>
+                    </div>
+                    <div>
+                        <a href="#ProjectsSection"><li className="firstLi">Projekte</li></a>
+                        <a href="#ProjectsSection"><li className="secondLi">项目</li></a>
+                    </div>
+                    <div>
+                        <a href="#KontaktSection"><li className="firstLi">Kontakt</li></a>
+                        <a href="#KontaktSection"><li className="secondLi">联系</li></a>
+                    </div>
+                </ul>
+                <div className="burger" onClick={showNav}><i className="fa fa-solid fa-bars"></i></div>
+            </div>
 
-        <div className="mobileNav">
-            <ul>
-                <div>
-                    <a href="#StartseiteSection"><li className="firstLi">Startseite | 主页</li></a>
-                </div>
-                <div>
-                    <a href="#AboutSection"><li className="firstLi">Über mich | 关于我</li></a>
-                </div>
-                <div>
-                    <a href="#SkillsSection"><li className="firstLi">Skills | 技能</li></a>
-                </div>
-                <div>
-                    <a href="#ProjectsSection"><li className="firstLi">Projekte | 项目</li></a>
-                </div>
-                <div>
-                    <a href="#KontaktSection"><li className="firstLi">Kontakt | 联系</li></a>
-                </div>
-            </ul>
-        </div>
+            <div className="mobileNav">
+                <ul>
+                    <div>
+                        <a href="#StartseiteSection"><li className="firstLi">Startseite | 主页</li></a>
+                    </div>
+                    <div>
+                        <a href="#AboutSection"><li className="firstLi">Über mich | 关于我</li></a>
+                    </div>
+                    <div>
+                        <a href="#SkillsSection"><li className="firstLi">Skills | 技能</li></a>
+                    </div>
+                    <div>
+                        <a href="#ProjectsSection"><li className="firstLi">Projekte | 项目</li></a>
+                    </div>
+                    <div>
+                        <a href="#KontaktSection"><li className="firstLi">Kontakt | 联系</li></a>
+                    </div>
+                </ul>
+            </div>
         </>
     )
 }
