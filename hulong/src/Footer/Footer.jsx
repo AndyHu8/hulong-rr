4b96c96fa954d49c69390f75457cf1343125dcc5
AndyHu8 <huandy01@gmail.com>
2023-01-23 18:30:31 +0100

    Impressum Link changed

--- hulong/src/Footer/Footer.jsx
+++ hulong/src/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className="footerContainer">
             <div>Entwickelt mit ganz viel Liebe und "schnellem" Internet in Berlin, Deutschland.</div>
             <div className="chinFooterText">在德国柏林，用大量的爱和 "快速" 的互联网开发。</div>
-            <div className="impressumFooterText" onClick={() => window.open(`${Pdf}`, '_blank')}><span>Zum Impressum</span></div>
+            <div className="impressumFooterText"><span onClick={() => window.open(`${Pdf}`, '_blank')}>Zum Impressum</span></div>
         </div>
     )
 }
