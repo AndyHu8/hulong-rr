6912b8685eb75230acc1a22bcb95c9625bb0d181
AndyHu8 <huandy01@gmail.com>
2023-02-12 01:03:38 +0100

    Weitere kleine Fixes

--- hulong/src/Footer/Footer.jsx
+++ hulong/src/Footer/Footer.jsx
@@ -6,6 +6,8 @@ export default function Footer() {
         <div className="footerContainer">
             <div>Entwickelt mit ganz viel Liebe und "schnellem" Internet in Berlin, Deutschland.</div>
             <div className="chinFooterText">在德国柏林，用大量的爱和 "快速" 的互联网开发。</div>
+            <p>© 2023 XIAØHU</p>
+            <br />
             <div className="impressumFooterText"><span onClick={() => window.open(`${Pdf}`, '_blank')}>Zum Impressum</span></div>
         </div>
     )
