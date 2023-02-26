5e75e31295e481cc47dbcd2c2699356cb6db35fd
AndyHu8 <huandy01@gmail.com>
2023-02-26 21:17:11 +0100

    Kleiner fix für Preis & Projects

--- hulong/src/Preise/Preise.jsx
+++ hulong/src/Preise/Preise.jsx
@@ -11,9 +11,9 @@ export default function Preise() {
 
             <div className="preisUebersicht">
                 <div className="flexContainer">
-                    <EinPreis headline="OnePager" preis="mind. 10" blur={false} desc="OnePager sind Webseiten, wie diese. Sie sind simpel angeordnet & der ganze Content ist auf einer übersichtlichen Seite aufgebaut. Solche sind ideal für User, die nicht vom Thema abschweifen sollen, in Unterseiten nicht verlaufen sollen & den Lesefluss nicht stören sollen."/>
-                    <EinPreis headline="Homepage" preis="mind. 15" blur={false} desc="Homepages mit Unterseiten sind gut geeignet für ausführlichen Inhalten, die ein Thema tiefer durchdringen. Außerdem schließt diese Art die Zielgruppe ältere Menschen ein. Diese sind oft mit Menüs groß geworden. Sie erwarten “unten auf einer Seite” keine anderen Themen als oben."/>
-                    <EinPreis headline="Online Shop" preis="mind. 25" blur={true} desc="Bald verfügbar."/>
+                    <EinPreis headline="OnePager" preis="mind. 50" blur={false} desc="OnePager sind Webseiten, wie diese. Sie sind simpel angeordnet & der ganze Content ist auf einer übersichtlichen Seite aufgebaut. Solche sind ideal für User, die nicht vom Thema abschweifen sollen, in Unterseiten nicht verlaufen sollen & den Lesefluss nicht stören sollen."/>
+                    <EinPreis headline="Homepage" preis="mind. 75" blur={false} desc="Homepages mit Unterseiten sind gut geeignet für ausführlichen Inhalten, die ein Thema tiefer durchdringen. Außerdem schließt diese Art die Zielgruppe ältere Menschen ein. Diese sind oft mit Menüs groß geworden. Sie erwarten “unten auf einer Seite” keine anderen Themen als oben."/>
+                    <EinPreis headline="Online Shop" preis="mind. 100" blur={true} desc="Bald verfügbar."/>
                 </div>
                 <br />
                 <p>Falls Sie zurzeit eine Website benötigen & diese Preise in Ordnung sind, zögern Sie nicht mich zu kontaktieren. Meine Kontakte finden Sie einen Reite drunter.</p>
